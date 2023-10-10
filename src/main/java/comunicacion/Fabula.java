6a9473433793b7a63125c973d1edca61c2fd7d83
SamuelCastaAlf <92555846+SamuelCastaAlf@users.noreply.github.com>
2023-10-10 05:29:01 -0500

    Error en digitación

--- src/main/java/comunicacion/Fabula.java
+++ src/main/java/comunicacion/Fabula.java
@@ -1,20 +1,20 @@
 package comunicacion;
 public class Fabula extends Escrito{
-	private String enseñanza;
+	private String ensenanza;
 	private String interpretacion;
 	
-	public Fabula(String origen,String titulo,String autor,int paginas,String enseñanza,String interpretacion){
+	public Fabula(String origen,String titulo,String autor,int paginas,String ensenanza,String interpretacion){
 		super(origen,titulo,autor,paginas);
-		this.enseñanza = enseñanza;
+		this.ensenanza = ensenanza;
 		this.interpretacion = interpretacion;
 	}
 	
-	public String getEnseñanza(){
-		return this.enseñanza;
+	public String getEnsenanza(){
+		return this.ensenanza;
 	}
 
-	public void setEnsenanza(String enseñanza){
-		this.enseñanza = enseñanza;
+	public void setEnsenanza(String ensenanza){
+		this.ensenanza = ensenanza;
 	}
 
 	public String getInterpretacion(){
@@ -35,6 +35,6 @@ public class Fabula extends Escrito{
 
     @Override
 	public String toString(){
-		return (this.getOrigen()+"\n"+this.getTitulo()+"\n"+this.getAutor()+"\n"+this.getPaginas()+"\n"+this.getEnseñanza());
+		return (this.getOrigen()+"\n"+this.getTitulo()+"\n"+this.getAutor()+"\n"+this.getPaginas()+"\n"+this.getEnsenanza());
 	}
 }
